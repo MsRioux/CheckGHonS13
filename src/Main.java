b2158750ba537cc6196638ea51f4ef62ec36bcc9
MsRioux <jrioux@mvusd.net>
2022-11-15 13:16:39 -0800

    Check Git Credential Manager is toggled on.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("YY U R");
         System.out.println("YY U B");
+        System.out.println("I C U R");
+        System.out.println("YY 4 Me");
     }
 }
