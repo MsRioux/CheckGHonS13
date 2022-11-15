85f5adb0dbcc4918d3b4c410f8d4a6809bfbc829
Rioux <jrioux@mvusd.net>
2022-11-15 12:17:44 -0800

    Initial commit - on S13 (Nayeli's) I had to download IntelliJ and GitBash and then go through Settings -> VC to put the path to Git in place.

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("YY U R");
+        System.out.println("YY U B");
+    }
+}
